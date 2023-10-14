8984f1e9f73a7fda6302cfbd8f64545479f601e4
Vastav <shrivastav.shivam1803@gmail.com>
2023-10-14 09:18:50 +0530

    09

---
+++ part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Conversation {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Greetings! How are you doing?");
+        String greetingReply = scanner.nextLine();
+//        System.out.println(greetingReply);
+        
+        
+        System.out.println("Oh, how interesting. Tell me more!");
+        String moreReply = scanner.nextLine();
+//        System.out.println(moreReply);
+      
+        
+        System.out.println("Thanks for sharing!");
+        
+        // Write your program here
+
+    }
+}
