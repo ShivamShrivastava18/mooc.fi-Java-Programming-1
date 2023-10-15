99d876b6f04f70af93d5aa42cc1d00dc05d6714e
Vastav <shrivastav.shivam1803@gmail.com>
2023-10-15 21:50:59 +0530

    20

---
+++ part02-Part02_20.RepeatingBreakingAndRemembering/src/main/java/RepeatingBreakingAndRemembering.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class RepeatingBreakingAndRemembering {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give numbers:");
+        int sum = 0;
+        int count = 0;
+        int evenCounter = 0;
+        int oddCounter = 0;
+        int userInput = Integer.valueOf(scanner.nextLine());
+        while (userInput != -1) {
+            sum += userInput;
+            count++;
+            
+            if(userInput % 2 == 0){
+                evenCounter++;
+            } else {
+                oddCounter++;
+            }
+            userInput = Integer.valueOf(scanner.nextLine());
+        }
+        double average = 1.0 * sum / count;      
+        System.out.println("Thx! Bye!");      
+        System.out.println("Sum:" + sum);       
+        System.out.println("Numbers: " + count);
+        System.out.println("Average: " + average);     
+        System.out.println("Even: " + evenCounter);
+        System.out.println("Odd: " + oddCounter);
+    }
+}
